c8e0e953692de9211846c934f0c54555026b71b1
soygagi <101847092+soygagi@users.noreply.github.com>
2022-11-24 19:30:39 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+diferencia = 0;
+noseX = 0;
+noseY = 0;
+derechaX = 0;
+izquierdaX = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -12,8 +17,17 @@ function modelLoaded(){
 function gotPoses(results){
    if(results.length>0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        izquierdaX = results[0].pose.leftWrist.x;
+        derechaX = results[0].pose.rightWrist.x;
+        diferencia = floor(izquierdaX - derechaX);
    }
 }
 function draw(){
     background('grey');
+    document.getElementById("square_side").innerHTML = "El alto y ancho del cuadardo sera = "+ diferencia + "px";
+    fill('#66FF66');
+    stroke('#0000FF');
+    square(noseX, noseY, diferencia);
 }
